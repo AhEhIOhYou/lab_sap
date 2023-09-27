45c5cbc3046480be3098b320a0113c71c779757e
ahehiohyou <vladimirov_d1ma@mail.ru>
2023-09-27 17:36:40 +0300

    lab5 update

--- lab5/lab5.go
+++ lab5/lab5.go
@@ -1,48 +1,99 @@
 package lab5
 
 import (
+	"math"
 	"sort"
-	"sync"
 )
 
-// median которая вычисляет медиану среза чисел
-func median(nums []float64) float64 {
-	n := len(nums)
+// FindMedian - параллельное вычисление медианного значения числовой последовательности.
+func FindMedian(arr []int) int {
+	n := len(arr)
 	if n == 0 {
 		return 0
 	}
-	sort.Float64s(nums)
-	if n%2 == 0 {
-		return (nums[n/2-1] + nums[n/2]) / 2
-	} else {
-		return nums[n/2]
+	if n == 1 {
+		return arr[0]
 	}
+
+	ch := make(chan int)
+
+	go func() {
+		sort.Ints(arr)
+		ch <- arr[n/2]
+	}()
+
+	return <-ch
 }
 
-// ParallelMedian вычисляет параллельно медиану каждого подсреза среза nums и возвращает их в срезе medians
-func ParallelMedian(nums []float64, n int) []float64 {
-	var wg sync.WaitGroup
-	ch := make(chan float64, n)
-	m := len(nums) / n
-	for i := 0; i < n; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			start := i * m
-			end := start + m
-			if i == n-1 {
-				end = len(nums)
-			}
-			sub := nums[start:end]
-			ch <- median(sub)
-		}(i)
-	}
-	wg.Wait()
-	close(ch)
-	var medians []float64
-	for m := range ch {
-		medians = append(medians, m)
+// CalculateNorm - параллельное вычисление нормы квадратной матрицы.
+func CalculateNorm(matrix [][]float64) float64 {
+	n := len(matrix)
+	if n == 0 {
+		return 0
 	}
 
-	return medians
+	ch := make(chan float64)
+
+	for i := 0; i < n; i++ {
+		go func(row []float64) {
+			norm := calculateRowNorm(row)
+			ch <- norm
+		}(matrix[i])
+	}
+
+	norms := make([]float64, n)
+	for i := 0; i < n; i++ {
+		norms[i] = <-ch
+	}
+
+	sum := 0.0
+	for _, value := range norms {
+		sum += value * value
+	}
+
+	return math.Sqrt(sum)
+}
+
+func calculateRowNorm(row []float64) float64 {
+	norm := 0.0
+	for _, value := range row {
+		norm += value * value
+	}
+	return math.Sqrt(norm)
+}
+
+// FindMinMax - параллельное вычисление максимального и минимального элементов одномерного массива
+func FindMinMax(arr []int) (int, int) {
+	n := len(arr)
+	if n == 0 {
+		return 0, 0
+	}
+	if n == 1 {
+		return arr[0], arr[0]
+	}
+
+	chMin := make(chan int)
+	chMax := make(chan int)
+
+	go func() {
+		min := arr[0]
+		for _, v := range arr {
+			if v < min {
+				min = v
+			}
+		}
+		chMin <- min
+	}()
+
+	go func() {
+		max := arr[0]
+		for _, v := range arr {
+			if v > max {
+				max = v
+			}
+		}
+		chMax <- max
+	}()
+
+	return <-chMin, <-chMax
 }
--- lab5/lab5_test.go
+++ lab5/lab5_test.go
@@ -2,30 +2,37 @@ package lab5_test
 
 import (
 	"lab5"
-	"reflect"
 	"testing"
 )
 
-func TestParallelMedian(t *testing.T) {
-	tests := []struct {
-		name string
-		nums []float64
-		n    int
-		want []float64
-	}{
-		{"Empty slice", []float64{}, 1, []float64{}},
-		{"One element", []float64{1.5}, 1, []float64{1.5}},
-		{"Even length", []float64{1.2, 3.4, 5.6, 7.8}, 2, []float64{2.3, 6.7}},
-		{"Odd length", []float64{1.2, 3.4, 5.6, 7.8, 9.0}, 3, []float64{1.2, 5.6, 9.0}},
-		{"Unsorted slice", []float64{2.4, 6.8, 4.5, 8.9, 10.1}, 4, []float64{2.4, 4.5, 8.9, 10.1}},
+func TestFindMedian(t *testing.T) {
+	arr := []int{-1, 0, 22, 34, 50}
+	median := lab5.FindMedian(arr)
+	if median != 22 {
+		t.Errorf("Expected minimum value to be 22 but got %d", median)
 	}
+}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := lab5.ParallelMedian(tt.nums, tt.n)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ParallelMedian(%v, %d) = %v, want %v", tt.nums, tt.n, got, tt.want)
-			}
-		})
+func TestCalculateNorm(t *testing.T) {
+	matrix := [][]float64{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	norm := lab5.CalculateNorm(matrix)
+	expectedNorm := 16.881943016134134
+	if norm != expectedNorm {
+		t.Errorf("Expected to be %f, but got %f", expectedNorm, norm)
+	}
+}
+
+func TestFindMinMax(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5}
+	min, max := lab5.FindMinMax(arr)
+	if min != 1 {
+		t.Errorf("Expected minimum value to be 1 but got %d", min)
+	}
+	if max != 5 {
+		t.Errorf("Expected maximum value to be 5 but got %d", max)
 	}
 }
